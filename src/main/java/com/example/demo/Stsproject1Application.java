2d7b2b4d918b114e6a1f91ee1ee62da57dfb3eb2
voidname <voidpurplesky@gmail.com>
2024-09-05 11:04:33 +0900

    first commit - STS Project

---
+++ src/main/java/com/example/demo/Stsproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stsproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stsproject1Application.class, args);
+	}
+
+}
